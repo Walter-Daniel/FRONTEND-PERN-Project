3c45205fcb1bdedc1a875ab7adf6f2061d0c7a5f
Walter <walterdcarrizo.19@gmail.com>
2024-03-31 11:18:41 -0300

    Se crearon formulario y manejos de errores

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { Layout } from './layout/Layout';
 import { NewProducts, Products } from './views';
+import { action as newProductAction } from './views/NewProducts';
+
 
 export const router = createBrowserRouter([
     {
@@ -13,7 +15,8 @@ export const router = createBrowserRouter([
             },
             {
                 path:'productos/nuevo',
-                element: <NewProducts />
+                element: <NewProducts />,
+                action: newProductAction
             }
         ]
     }
